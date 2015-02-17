c3ecb2239750069fbb59c0ca15b87195ded70b8d
emna.slama <emna.slama@esprit.tn>
2015-02-17 11:07:26 +0100

    Action 1

--- src/main/java/com/esprit/naturespirit/domain/Action.java
+++ src/main/java/com/esprit/naturespirit/domain/Action.java
@@ -1,5 +1,6 @@
 package com.esprit.naturespirit.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -10,7 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
-public class Action {
+public class Action implements Serializable{
 
 	private int id;
 	private String desciption;
